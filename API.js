25a3055062c00de057d60dcb85ee6f27aca8e206
Martin Delobel <martin.delobel@decathlon.com>
2017-12-22 17:07:21 +0100

    files names

--- API.js
+++ API.js
@@ -1,58 +1,31 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
-const server = require("./server.js");
+const fetch = require("node-fetch");
 
-const app = express();
-const port = process.env.PORT || 3000;
+function getCategories() {
+  return fetch("https://decath-product-api.herokuapp.com/categories")
+    .then((res) => res.json())
+    .catch((error) => {
+      console.warn(error);
+    });
+}
 
-nunjucks.configure("views", {
-  autoescape: true,
-  express: app
-});
+function getProductsByCategory(idCategory) {
+  return fetch(`https://decath-product-api.herokuapp.com/categories/${idCategory}/products`)
+    .then((res) => res.json())
+    .catch((error) => {
+      console.warn(error);
+    });
+}
 
-app.use(express.static("images"));
+function getProducts(idProduct) {
+  return fetch(`https://decath-product-api.herokuapp.com/products/${idProduct}`)
+    .then((res) => res.json())
+    .catch((error) => {
+      console.warn(error);
+    });
+}
 
-app.set("views", __dirname + "/views");
-app.set("view engine", "njk");
-
-app.listen(port, function () {
-  console.log("Server listening on port:" + port);
-});
-
-app.get("/", function(request, result) {
-  server.getCategories()
-  .then(function(categories) {
-    result.render("categories_list", {
-      categories : categories
-      }
-    )
-  })
-});
-
-app.get("/category/:idCategory/products", function(request, result) {
-  Promise.all(
-    [
-      server.getProductsByCategory(request.params.idCategory),
-      server.getCategories(),
-    ]
-  )
-  .then(function(promiseAllResult) {
-      const id = request.params.idCategory;
-      const cat = promiseAllResult[1].filter(category => category.id === id)
-      result.render("category", {
-        products : promiseAllResult[0],
-        categories : promiseAllResult[1],
-        category : cat
-      })
-  })
-});
-
-app.get("/products/:idProduct", function(request, result) {
-  server.getProducts(request.params.idProduct)
-  .then(function(product) {
-    result.render("product", {
-      product : product
-      }
-    )
-  })
-});
+module.exports = {
+  getCategories: getCategories,
+  getProductsByCategory: getProductsByCategory,
+  getProducts: getProducts
+}
